610dd95deed40c4e443f034836e03ab1eb906841
Kardantel <carlosdpimenteld@gmail.com>
2020-09-18 14:52:10 -0500

    Agregué función dot_product.

--- multmat.py
+++ multmat.py
@@ -8,3 +8,10 @@ def get_column(matrix, column_number):
         n = matrix[i][column_number]
         column.append(n)
     return column
+
+
+def dot_product(vector_one, vector_two):
+    result = 0
+    for i in range(len(vector_one)):
+        result += vector_one[i] * vector_two[i]
+    return result
