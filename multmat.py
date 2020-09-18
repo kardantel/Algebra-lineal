92ba7f3c76959177d2644c3f2ab8a360a191b6c5
Kardantel <carlosdpimenteld@gmail.com>
2020-09-18 14:51:38 -0500

    Agregué función get_column.

--- multmat.py
+++ multmat.py
@@ -1,2 +1,10 @@
 def get_row(matrix, row):
     return matrix[row]
+
+
+def get_column(matrix, column_number):
+    column = []
+    for i in range(len(matrix)):
+        n = matrix[i][column_number]
+        column.append(n)
+    return column
